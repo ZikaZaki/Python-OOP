a543573459c22177d3c1c657dbb6d5a67493a95c
ZikaZaki <zika.github@gmail.com>
2022-04-04 23:09:32 +0300

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -12,8 +12,11 @@ Steve = Employee()
 Steve.ID = 3789
 Steve.salary = 2500
 Steve.department = "Human Resources"
+# creating a new attribute for Steve
+Steve.title = "Manager"
 
 # printing properties of Steve
 print("ID: ", Steve.ID)
 print("Salary: ", Steve.salary)
 print("Department: ", Steve.department)
+print("Title: ", Steve.title)

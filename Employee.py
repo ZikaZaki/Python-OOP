663ba7f1ebad7b764ad2bd1f9b41bf3ac20caa8d
ZikaZaki <zika.github@gmail.com>
2022-04-04 23:06:54 +0300

    Update Employee.py

--- Employee.py
+++ Employee.py
@@ -3,3 +3,17 @@ class Employee:
     ID = None
     salary = None
     department = None
+
+
+# creating an object of the Employee class
+Steve = Employee()
+
+# assigning values to properties of Steve - an object of the Employee class
+Steve.ID = 3789
+Steve.salary = 2500
+Steve.department = "Human Resources"
+
+# printing properties of Steve
+print("ID: ", Steve.ID)
+print("Salary: ", Steve.salary)
+print("Department: ", Steve.department)

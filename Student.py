2f27ea455f077c76f465256643cf2bbb7ff27795
ZikaZaki <zika.github@gmail.com>
2022-04-05 23:15:43 +0300

    Add Student Class

---
+++ Student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__(self):
+        self.__name = None
+        self.__rollNumber = None
+
+    def setName(self, name):
+        self.__name = name
+
+    def getName(self):
+        return self.__name
+
+    def setRollNumber(self, rollNumber):
+        self.__rollNumber = rollNumber
+
+    def getRollNumber(self):
+        return self.__rollNumber

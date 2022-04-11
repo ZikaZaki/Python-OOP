fb1745a8716679796ccbcf746634242a5ee80b27
ZikaZaki <zika.github@gmail.com>
2022-04-12 01:16:47 +0300

    Added Python-Closures Directory

---
+++ Python-Closures/main.py
@@ -0,0 +1,17 @@
+def print_msg(msg):
+    # This is the outer enclosing function
+
+    def printer():
+        # This is the nested function
+        print(msg)
+
+    return printer  # returns the nested function
+
+
+# Now let's try calling this function.
+# Output: Hello
+another = print_msg("Hello")
+another()
+
+del print_msg
+another()

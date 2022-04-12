b1dcb365bcd9163b4aef00fe0c3b24827135a14d
ZikaZaki <zika.github@gmail.com>
2022-04-13 01:15:53 +0300

    Updated main.py

--- Python-Closures/main.py
+++ Python-Closures/main.py
@@ -1,3 +1,6 @@
+from os import times
+
+
 def print_msg(msg):
     # This is the outer enclosing function
 
@@ -15,3 +18,30 @@ another()
 
 del print_msg
 another()
+
+
+def make_multiplier_of(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+
+# Multiplier of 3
+times3 = make_multiplier_of(3)
+
+# Multiplier of 5
+times5 = make_multiplier_of(5)
+
+# Output: 27
+print(times3(9))
+
+# Output: 15
+print(times5(3))
+
+# Output: 30
+print(times5(times3(2)))
+
+
+print(make_multiplier_of.__closure__)
+
+print(times3.__closure__)

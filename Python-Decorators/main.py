6f12485bce92b9249394a70dd816aacf05539df8
ZikaZaki <zika.github@gmail.com>
2022-04-14 01:01:02 +0300

    Updated main.py

--- Python-Decorators/main.py
+++ Python-Decorators/main.py
@@ -40,3 +40,53 @@ def display_info(func):
 
 decorated_func = display_info(printer)
 decorated_func()
+
+
+def smart_divide(func):
+    def inner(a, b):
+        print("Dividing: ", a, "by", b)
+        if b == 0:
+            print("Cannot divide by 0!")
+            return
+        return func(a, b)
+    return inner
+
+
+@smart_divide
+def divide(a, b):
+    return a / b
+
+
+print(divide(15, 3))
+
+print(divide(15, 0))
+
+
+def star(func):
+    def inner(*args, **kwargs):
+        print("*" * 30)
+        func(*args, **kwargs)
+        print("*" * 30)
+    return inner
+
+
+def percent(func):
+    def inner(*args, **kwargs):
+        print("%" * 30)
+        func(*args, **kwargs)
+        print("%" * 30)
+    return inner
+
+
+@star
+@percent
+def printer(msg):
+    print(msg)
+
+
+printer("Decorators are cool!")
+
+
+# The above example is equivalent to the following:
+# printer = star(percent(printer))
+star(percent(printer("Decorators are cool!")))
